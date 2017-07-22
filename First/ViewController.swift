6abd0261ce5a44cadf93369b56b6b5bad2411c3a
mani <mani@manis-MacBook-Pro.local>
2017-07-22 17:30:25 +0530

    Changes

--- First/ViewController.swift
+++ First/ViewController.swift
@@ -10,13 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var stopRecord: UIButton!
     @IBOutlet weak var statusLabel: UILabel!
     @IBOutlet weak var recordButton: UIButton!
-    @IBOutlet weak var stopRecord: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         statusLabel.text = ""
+        stopRecord.enabl
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,11 +26,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func recordAudio(_ sender: UIButton) {
+    @IBAction func recordAudio(_ sender: Any) {
         setStatus(status: "Recording..");
+
     }
 
-    @IBAction func stopRecording(_ sender: UIButton) {
+
+    @IBAction func stopRecord(_ sender: Any) {
         setStatus(status: "Stopped");
     }
     
